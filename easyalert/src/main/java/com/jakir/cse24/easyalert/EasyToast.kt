b5466a20b0ddf2b59aa956d3b15e27a963608e25
Jakir Hossain <jakir.cse24@gmail.com>
2019-12-19 12:58:17 +0600

    added info, warning toast and default toast with icon

--- easyalert/src/main/java/com/jakir/cse24/easyalert/EasyToast.kt
+++ easyalert/src/main/java/com/jakir/cse24/easyalert/EasyToast.kt
@@ -23,7 +23,7 @@ object EasyToast {
         val toast: Toast = customToast(
             context,
             msg,
-            R.color.color_white,
+            R.color.color_black,
             false,
             R.drawable.ic_check_white_24dp,
             R.color.color_white,
@@ -33,11 +33,50 @@ object EasyToast {
     }
 
     /**
+     * This method is for showing message using android [Toast]
+     * @author Md. Jakir Hossain on 29/04/2019.
+     *
+     * @param msg message which will display on screen
+     */
+    fun showToastWithIcon(context: Context, msg: String, icon: Int) {
+        val toast: Toast = customToast(
+            context,
+            msg,
+            R.color.color_black,
+            true,
+            icon,
+            R.color.color_white,
+            R.color.color_grey
+        )
+        toast.show()
+    }
+
+    /**
+     * This function is for showing Info [Toast].
+     * @author Md. Jakir Hossain on 18/12/2019.
+     *
+     * @param context application context.
+     * @param msg message that should be shown in [Toast].
+     */
+    fun showInfoToast(context: Context, msg: String) {
+        val toast: Toast = customToast(
+            context,
+            msg,
+            R.color.color_white,
+            true,
+            R.drawable.ic_info_outline_white_24dp,
+            R.color.color_white,
+            R.color.color_info
+        )
+        toast.show()
+    }
+
+    /**
      * This function is for showing success [Toast].
      * @author Md. Jakir Hossain on 18/12/2019.
      *
      * @param context application context.
-     * @param msg message that should be shown in toast.
+     * @param msg message that should be shown in [Toast].
      */
     fun showSuccessToast(context: Context, msg: String) {
         val toast: Toast = customToast(
@@ -53,11 +92,31 @@ object EasyToast {
     }
 
     /**
-     * This function is for showing error [Toast].
+     * This function is for showing Info [Toast].
      * @author Md. Jakir Hossain on 18/12/2019.
      *
      * @param context application context.
-     * @param msg message which should be shown in toast.
+     * @param msg message that should be shown in [Toast].
+     */
+    fun showWarningToast(context: Context, msg: String) {
+        val toast: Toast = customToast(
+            context,
+            msg,
+            R.color.color_white,
+            true,
+            R.drawable.ic_warning_white_24dp,
+            R.color.color_white,
+            R.color.color_warning
+        )
+        toast.show()
+    }
+
+    /**
+     * This function is for showing error [Toast].
+     * @author Md. Jakir Hossain on 18/12/2019.
+     *
+     * @param context application [Context].
+     * @param msg message which should be shown in [Toast].
      */
     fun showErrorToast(context: Context, msg: String) {
         val toast: Toast = customToast(
@@ -76,14 +135,14 @@ object EasyToast {
      * This function is for creating Custom [Toast]
      * @author Md. Jakir Hossain on 18/12/2019.
      *
-     * @param context application context.
+     * @param context application [Context].
      * @param text message for showing in toast.
      * @param textColor message text color.
      * @param hasIcon is toast has icon or not.
      * @param icon icon id(int).
      * @param iconColor icon tint color.
      * @param bgColor toast background color.
-     * @return an instance of {@link Toast}.
+     * @return an instance of [Toast].
      */
     private fun customToast(
         context: Context,
