13c25637e6cac8f065761529a8481f4e69a68edc
Jakir Hossain <jakir.cse24@gmail.com>
2019-12-19 12:53:59 +0600

    added button to show alert, logs and toast

--- app/src/main/java/com/jakir/cse24/easyalert/MainActivity.kt
+++ app/src/main/java/com/jakir/cse24/easyalert/MainActivity.kt
@@ -2,13 +2,22 @@ package com.jakir.cse24.easyalert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        EasyLog.logD("MainActivity","message")
-        EasyToast.showErrorToast(this,"Hello Toast!")
+
+    }
+
+    fun show(view: View) {
+        EasyLog.logV("MainActivity","Verbose message")
+        EasyLog.logI("MainActivity","Info message")
+        EasyLog.logD("MainActivity","Debug message")
+        EasyLog.logW("MainActivity","Debug message")
+        EasyLog.logE("MainActivity","Error message")
+        EasyToast.showToast(this,"Hello Toast!")
     }
 }
