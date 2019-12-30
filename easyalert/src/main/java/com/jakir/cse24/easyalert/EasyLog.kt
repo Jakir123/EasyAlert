da0655deed279672c4c3e83b5e48827f7587b2d8
Jakir Hossain <jakir.cse24@gmail.com>
2019-12-30 14:48:41 +0600

    Make tag optional for Logs. Example: EasyLog.logV("log message", "log tag") or EasyLog.logV("log message").

--- easyalert/src/main/java/com/jakir/cse24/easyalert/EasyLog.kt
+++ easyalert/src/main/java/com/jakir/cse24/easyalert/EasyLog.kt
@@ -7,10 +7,10 @@ object EasyLog {
      * This method is for printing a [ERROR] log message on the system console
      * @author Md. Jakir Hossain on 02/05/2019.
      *
-     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      * @param msg The message you would like logged.
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      */
-    fun logE(tag: String, msg: String) {
+    fun logE(msg: String, tag: String = "") {
         Log.e("EasyAlert $tag", msg)
     }
 
@@ -18,10 +18,10 @@ object EasyLog {
      * This method is for printing a [DEBUG] log message on the system console
      * @author Md. Jakir Hossain on 02/05/2019.
      *
-     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      * @param msg The message you would like logged.
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      */
-    fun logD(tag: String, msg: String) {
+    fun logD(msg: String, tag: String="") {
         Log.d("EasyAlert $tag", msg)
     }
 
@@ -29,10 +29,10 @@ object EasyLog {
      * This method is for printing a [INFO] log message on the system console
      * @author Md. Jakir Hossain on 19/12/2019.
      *
-     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      * @param msg The message you would like logged.
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      */
-    fun logI(tag: String, msg: String) {
+    fun logI(msg: String, tag: String="") {
         Log.i("EasyAlert $tag", msg)
     }
 
@@ -40,10 +40,10 @@ object EasyLog {
      * This method is for printing a [WARNING] log message on the system console
      * @author Md. Jakir Hossain on 19/12/2019.
      *
-     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      * @param msg The message you would like logged.
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      */
-    fun logW(tag: String, msg: String) {
+    fun logW(msg: String, tag: String="") {
         Log.w("EasyAlert $tag", msg)
     }
 
@@ -51,10 +51,10 @@ object EasyLog {
      * This method is for printing a [VERBOSE] log message on the system console
      * @author Md. Jakir Hossain on 19/12/2019.
      *
-     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      * @param msg The message you would like logged.
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      */
-    fun logV(tag: String, msg: String) {
+    fun logV(msg: String, tag: String = "") {
         Log.v("EasyAlert $tag", msg)
     }
 }
